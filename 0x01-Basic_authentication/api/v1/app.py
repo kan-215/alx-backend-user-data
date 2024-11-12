1e66f9e563b0ba2f21a83fd7b0109a2b40dfc3fa
kan-215 <katamialvin369@gmail.com>
2024-11-12 23:37:10 +0300

    api

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,87 +1,87 @@
 #!/usr/bin/env python3
 """
-API route module
+Route module for the API
 """
+from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-from os import getenv
+import logging
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
+
+
+# logging.basicConfig(level=logging.DEBUG,filename='test.log')
 
 
 app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-AUTH_TYPE = getenv("AUTH_TYPE")
-
-if AUTH_TYPE == "auth":
-    from api.v1.auth.auth import Auth
-    auth = Auth()
-elif AUTH_TYPE == "basic_auth":
-    from api.v1.auth.basic_auth import BasicAuth
-    auth = BasicAuth()
-elif AUTH_TYPE == "session_auth":
-    from api.v1.auth.session_auth import SessionAuth
-    auth = SessionAuth()
-elif AUTH_TYPE == "session_exp_auth":
-    from api.v1.auth.session_exp_auth import SessionExpAuth
-    auth = SessionExpAuth()
-elif AUTH_TYPE == "session_db_auth":
-    from api.v1.auth.session_db_auth import SessionDBAuth
-    auth = SessionDBAuth()
 
 
-@ app.errorhandler(404)
+@app.errorhandler(404)
 def not_found(error) -> str:
-    """ Handles 'Not found'
+    """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
 
 
-@ app.errorhandler(401)
-def unauthorized_error(error) -> str:
-    """ Unauthorized error handler
-    """
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """unauthorized handler"""
     return jsonify({"error": "Unauthorized"}), 401
 
 
-@ app.errorhandler(403)
-def forbidden_error(error) -> str:
-    """ Forbidden error handler
-    """
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """restricted to some resources (403)"""
     return jsonify({"error": "Forbidden"}), 403
 
 
-@ app.before_request
-def before_request() -> str:
-    """ Before Request Handler
-    Requests Validation
+@app.before_request
+def before_request():
+    """ Filter each request before handling it
     """
     if auth is None:
         return
-
-    excluded_paths = ['/api/v1/status/',
-                      '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/',
-                      '/api/v1/auth_session/login/']
-
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
     if not auth.require_auth(request.path, excluded_paths):
         return
-
-    if auth.authorization_header(request) is None \
-            and auth.session_cookie(request) is None:
+    if auth.authorization_header(request) is None:
         abort(401)
-
-    current_user = auth.current_user(request)
-    if current_user is None:
+    if auth.current_user(request) is None:
         abort(403)
 
-    request.current_user = current_user
+
+@app.before_request
+def before_request():
+    '''called before any request is implemented
+    '''
+    auth = getenv('AUTH_TYPE')
+    if auth == 'basic_auth':
+        from api.v1.auth.basic_auth import BasicAuth
+        auth = BasicAuth()
+    elif auth is not None:
+        auth = Auth()
+    if auth is not None and auth.require_auth(request.path,
+                                              ['/api/v1/status/',
+                                               '/api/v1/unauthorized/',
+                                               '/api/v1/forbidden/']):
+        header = auth.authorization_header(request)
+        if header is None:
+            abort(401)
+        user = auth.current_user(request)
+        if user is None:
+            abort(403)
 
 
 if __name__ == "__main__":
+    # for rule  in app.url_map.iter_rules():
+    #     print(rule)
+
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
